13b7471d74caee660bf394cea3558cf79ca88f86
davidonelson <david@goism.com>
2010-10-31 00:00:33 -0700

    testing again

--- Application1/Form1.cs
+++ Application1/Form1.cs
@@ -25,5 +25,10 @@ namespace Application1
         {
             MessageBox.Show("goodbye");
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("good day");
+        }
     }
 }
