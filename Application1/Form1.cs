c29dc511d0948f43eddf1eae6f304a518c395cdb
davidonelson <david@goism.com>
2010-10-30 21:50:37 -0700

    change

--- Application1/Form1.cs
+++ Application1/Form1.cs
@@ -15,5 +15,10 @@ namespace Application1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello");
+        }
     }
 }
